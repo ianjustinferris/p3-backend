e9066a9c0f8d0739f272e8f8633e4afdeb58a294
ianjustinferris <ianjustinferris@gmail.com>
2022-08-07 11:56:48 -0400

    setup mongoDB and connection

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 8800;
 
 
 const app = express();
+connectDB();
 
 app.use('/graphql', graphqlHTTP({
     schema,
